ca8b34143e4fe876ed4fcae0e94d3a64bf7bf9ff
Aditya <65965165+08aditya@users.noreply.github.com>
2023-03-13 21:37:38 +0530

    final product

--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Carditem = () => {
+const Carditem = (props) => {
   return (
     <div>
-      <li className='cards__item'> </li>
-      <Link className='cards__item__link'>
-        <figure className = "cards__item__pic-wrap">
-            <img src='/' alt='Travel Image' className = "cards__item__img"></img>
-        </figure>
-        <div className='cards__item__info' >
-            <h5 className='cards__item__text'></h5>
-        </div>
-      </Link>
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+          <figure className="cards__item__pic-wrap" data-category={props.lable}>
+            <img
+              src={props.src}
+              alt="Travel "
+              className="cards__item__img"
+            ></img>
+          </figure>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
+          </div>
+        </Link>
+      </li>
     </div>
-  )
-}
+  );
+};
 
-export default Carditem
+export default Carditem;
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Carditem from "./Carditem";
 import "./Cards.css";
+import img9 from "../assets/images/img-9.jpg"
+import img2 from "../assets/images/img-2.jpg"
+import img3 from "../assets/images/img-3.jpg"
+import img4 from "../assets/images/img-4.jpg"
+import img8 from "../assets/images/img-8.jpg"
+
 
 const Cards = () => {
   return (
@@ -9,7 +15,43 @@ const Cards = () => {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <Carditem />
+            <Carditem
+              src={img9}
+              text="Explore The Hidden Waterfall In The Amazon Jungle "
+              lable="Adventure"
+              path="/services"
+              
+            />
+            <Carditem
+              src={img2}
+              text="Travel through the Islands of Bali in a Private Cruise "
+              lable="Luxury"
+              path="/services"
+              
+            />
+          </ul>
+          <ul className="cards__items">
+            <Carditem
+              src={img3}
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters "
+              lable="Mystry"
+              path="/services"
+              
+            />
+            <Carditem
+              src={img4}
+              text="Experience Football on Top of the Himilayan Mountains "
+              lable="Adventure"
+              path="/services"
+              
+            />
+            <Carditem
+              src={img8}
+              text="Ride through the Sahara Desert on a guided camel tour "
+              lable="Adrenaline"
+              path="/services"
+              
+            />
           </ul>
         </div>
       </div>
