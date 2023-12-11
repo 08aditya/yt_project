e044b609c1677961bc652d85e87856c451569132
Yashraj Singh <yashraj@appinventers.com>
2023-12-11 12:27:59 +0530

    update

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import "../../App.css"
 
-const Products = () => {
+const Products = () => { 
+  const [api, setApi] = useState();
+  const [Iserror, setIserror] = useState()
+  useEffect(() => {
+    axios.get("https://picsum.photos/v2/list")
+      .then((res) => {setApi(res.data )
+      // console.log("2",  );
+    })
+      .catch((error) => { setIserror(error.message) })
+  }, []);
+  console.log(api)
   return (
-    <div>
-      <h1 className='products'> products </h1>
+    <div className='products'>
+      <h1 > products </h1>
+      {/* api.map((res) =>  {
+        
+      }) */}
+      
     </div>
   )
 }
